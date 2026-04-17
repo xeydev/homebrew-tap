76377e59022b5049ac7e84163f4d3ce6cad009c3
xeydev <suhotrub1@gmail.com>
2026-04-17 01:22:51 +0100

    chore: add nanobar formula

---
+++ Formula/nanobar.rb
@@ -0,0 +1,45 @@
+class Nanobar < Formula
+  desc "Minimal macOS status bar app powered by SwiftUI plugins"
+  homepage "https://github.com/xeydev/nanobar"
+  version "1.0.0"
+  url "https://github.com/xeydev/nanobar/releases/download/v#{version}/nanobar-#{version}-arm64.tar.gz"
+  sha256 "PLACEHOLDER"
+
+  depends_on arch: :arm64
+  depends_on macos: :sequoia
+
+  def install
+    libexec.install "libexec/NanoBar"
+    libexec.install "libexec/NowPlayingHelper"
+    (libexec/"Plugins").mkpath
+    Dir["libexec/Plugins/*.bundle"].each do |bundle|
+      (libexec/"Plugins").install bundle
+    end
+    (bin/"nanobar").write_env_script libexec/"NanoBar", {}
+  end
+
+  service do
+    run opt_libexec/"NanoBar"
+    keep_alive true
+    log_path "/tmp/nanobar.log"
+    error_log_path "/tmp/nanobar.err"
+    run_at_load true
+    environment_variables PATH: std_service_path_env
+  end
+
+  def caveats
+    <<~EOS
+      Config: ~/.config/nanobar/config.toml (created on first run if absent)
+
+      Start:  brew services start nanobar
+      Stop:   brew services stop nanobar
+      Logs:   tail -f /tmp/nanobar.log
+    EOS
+  end
+
+  test do
+    assert_predicate libexec/"NanoBar", :executable?
+    assert_predicate libexec/"NowPlayingHelper", :executable?
+    assert_predicate libexec/"Plugins", :directory?
+  end
+end
